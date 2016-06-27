1350b5c6b68ea3d0ae5327a3fa0801daa3bf0008
xize <glucassen@live.nl>
2016-06-27 13:23:33 +0200

    fix Form1 refactor

--- src/Program.cs
+++ src/Program.cs
@@ -16,7 +16,7 @@ namespace Resolution_Grabber.src
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Window());
         }
     }
 }
