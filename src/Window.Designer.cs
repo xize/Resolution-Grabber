ad7d2101fea645b82834b95f56d47324e6b31320
xize <glucassen@live.nl>
2016-06-27 13:26:02 +0200

    change window type to SizeableWindow

--- src/Window.Designer.cs
+++ src/Window.Designer.cs
@@ -51,7 +51,7 @@
             | System.Windows.Forms.AnchorStyles.Right)));
             this.richTextBox1.Location = new System.Drawing.Point(12, 39);
             this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(432, 247);
+            this.richTextBox1.Size = new System.Drawing.Size(432, 251);
             this.richTextBox1.TabIndex = 1;
             this.richTextBox1.Text = "";
             // 
@@ -75,7 +75,7 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // Form1
+            // Window
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
@@ -84,8 +84,9 @@
             this.Controls.Add(this.label1);
             this.Controls.Add(this.richTextBox1);
             this.Controls.Add(this.textBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
             this.MinimumSize = new System.Drawing.Size(472, 336);
-            this.Name = "Form1";
+            this.Name = "Window";
             this.Text = "Resolution Grabber v1.2";
             this.ResumeLayout(false);
             this.PerformLayout();
