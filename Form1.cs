202b995d856baeaf2d0f1cd62efa6568aadfc643
xize <glucassen@live.nl>
2016-06-27 13:13:52 +0200

    add beginning of the project

---
+++ Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Resolution_Grabber
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(button1.Text == "check resolution")
+            {
+                textBox1.Enabled = false;
+                button1.Text = "stop";
+            } else
+            {
+                textBox1.Enabled = true;
+                button1.Text = "check resolution";
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
